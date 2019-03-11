97b7f62c5bbf95528878ef1ded9b9b97a0756134
zero_two <zero_two@localhost.localdomain>
2019-03-11 23:16:04 +0300

    Залито => рег форма создана

--- src/app/forms/MainForm.php
+++ src/app/forms/MainForm.php
@@ -3,8 +3,12 @@ namespace app\forms;
 
 use std, gui, framework, app;
 
+class MainForm extends AbstractForm {
 
-class MainForm extends AbstractForm
-{
-
+    /**
+     * @event buttonAlt.action 
+     */
+    function doButtonAltAction(UXEvent $e = null) {    
+        $this->form('register')->show();
+    }
 }
