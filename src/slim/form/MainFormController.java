0049a05fd5b309f450ae46e966381a4ba8e6b59d
zero_two <zero_two@localhost.localdomain>
2019-03-12 18:31:22 +0300

    действие на кнопки

--- src/slim/form/MainFormController.java
+++ src/slim/form/MainFormController.java
@@ -5,12 +5,14 @@
  */
 package slim.form;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
 
 /**
  * FXML Controller class
@@ -25,13 +27,18 @@ public class MainFormController implements Initializable {
     private Button Login;
     @FXML
     private Button register;
-
+    
+    @FXML
+    private void auth(javafx.event.ActionEvent event) {
+        System.out.print("hello");
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
 }
