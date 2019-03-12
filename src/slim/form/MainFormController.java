ae38ecd8c160841ba31e52172515ede8c56868a4
uxredbyte <bakmen2543@gmail.com>
2019-03-12 22:27:40 +0000

    Добавлена поля пароля

--- src/slim/form/MainFormController.java
+++ src/slim/form/MainFormController.java
@@ -12,8 +12,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 
 /**
  * FXML Controller class
@@ -28,6 +28,8 @@ public class MainFormController implements Initializable {
     private Button Login;
     @FXML
     private Button register;
+    @FXML
+    private PasswordField pass;
     
     @FXML
     private void auth(ActionEvent event) {
