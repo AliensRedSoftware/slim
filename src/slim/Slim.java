15acd26e353a9808575e7b6a68953c5772fe904b
uxredbyte <bakmen2543@gmail.com>
2019-03-17 16:08:58 +0000

    Авторизация аккаунт => Версия 0.1

--- src/slim/Slim.java
+++ src/slim/Slim.java
@@ -11,6 +11,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -19,17 +20,18 @@ import javafx.stage.Stage;
  */
 public class Slim extends Application {
     
+    
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Slim"); //Установка загаловка
-        stage.centerOnScreen(); //Показать по середине
-        stage.setResizable(false); //Убрать растягивание формы
+    public void start(Stage MainForm) throws Exception {
+        MainForm.setTitle("Slim"); //Установка загаловка
+        MainForm.centerOnScreen(); //Показать по середине
+        MainForm.setResizable(false); //Убрать растягивание формы
         Parent root = FXMLLoader.load(getClass().getResource("forms/MainForm.fxml"));
         
         Scene scene = new Scene(root);
         
-        stage.setScene(scene);
-        stage.show();
+        MainForm.setScene(scene);
+        MainForm.show();
     }
 
     /**
--- src/slim/forms/MainFormController.java
+++ src/slim/forms/MainFormController.java
@@ -9,17 +9,25 @@ import javafx.event.ActionEvent;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import slim.DBConnection;
+import slim.Slim;
 
 /**
  * FXML Controller class
@@ -39,9 +47,10 @@ public class MainFormController implements Initializable {
     
     private Connection con = null; 
     private PreparedStatement pst = null;
+    private ResultSet result = null;
     
     @FXML
-    private void auth(ActionEvent event) {
+    private void auth(ActionEvent event) throws Exception {
         String name = this.name.getText().trim();
         String password = this.pass.getText().trim();
         if (name.isEmpty()) {
@@ -51,7 +60,64 @@ public class MainFormController implements Initializable {
            new Alert(Alert.AlertType.ERROR, "Пожалуйста введите пароль для входа").showAndWait(); 
            this.pass.setText("");
         } else {
-            
+            try {
+                try {
+                    String sql = "SELECT * FROM `database`";
+                    Boolean successLogin = false;
+                    Boolean successPassword = false;
+                    pst = con.prepareStatement(sql);
+                    result = pst.executeQuery();
+                    while (result.next()) {
+                        String getName = result.getString(2);;
+                        if (name.equals(getName)) { //Проверка на логинность
+                            successLogin = true;
+                            break;
+                        } else {
+                            successLogin = false;
+                        }
+                    }
+                    result = pst.executeQuery(); //Перезагрузка запроса ;)
+                    while (result.next()){
+                        if (successLogin == true) {
+                            String getPassword = result.getString(3);
+                            if (password.equals(getPassword)) {
+                                successPassword = true;
+                                break;
+                            }
+                        } else {
+                            successLogin = false;
+                            successPassword = false;
+                            break;
+                        }
+                    }
+                    if (successLogin == true && successPassword == true) {
+                        System.out.println("[Аккаунт => успешный был вход!]");
+                        new Alert(Alert.AlertType.INFORMATION, "Успешный был вход!").showAndWait();
+                        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("chat.fxml"));
+                        Parent rootl = (Parent) fxmlloader.load();
+                        Stage stage = new Stage();
+                        stage.setTitle("Chat");
+                        stage.centerOnScreen(); //Показать по середине
+                        stage.setScene(new Scene(rootl));
+                        stage.show();
+                        Node source = (Node) event.getSource();
+                        Window MainForm = source.getScene().getWindow();
+                        MainForm.getScene().getWindow().hide();
+                    }
+                    if (successLogin == false) {
+                        System.out.println("[Аккаунт => неверный логин]");
+                        new Alert(Alert.AlertType.ERROR, "Неверный логин при входи!").showAndWait();    
+                    }
+                    if (successPassword == false) {
+                        System.out.println("[Аккаунт => неверный пароль]");
+                        new Alert(Alert.AlertType.ERROR, "Неверный пароль при входи!").showAndWait();
+                    }
+                } catch (SQLException ex) {
+                    
+                }
+            } catch (Exception e) {
+                System.out.println("Ошибка!");
+            }
         }
     }
     
@@ -87,8 +153,7 @@ public class MainFormController implements Initializable {
             finally {
                 pst.cancel();
             }
-        }
-        
+        } 
     }
     
     /**
